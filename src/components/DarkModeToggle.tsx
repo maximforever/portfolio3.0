d5d2b81d92f5f9c97e29b8e7f0d2248383e4167b
Max Pekarsky <pekarsky.maxim@gmail.com>
2022-07-29 12:06:36 -0400

    format darkModeToggle

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 
-const ImageIcon = styled.span<{darkMode: boolean}>`
+const ImageIcon = styled.span<{ darkMode: boolean }>`
   font-size: 1rem;
   &:hover {
-    color: ${(props) => props.darkMode ?
-      'yellow'
-      :
-      'blue'
-    };
+    color: ${(props) => props.darkMode ? 'yellow' : 'blue'};
     cursor: pointer;
   }
 `
 
-const DarkModeToggle: React.FunctionComponent<{ 
+const DarkModeToggle: React.FunctionComponent<{
   darkMode: boolean,
   changeMode: () => void,
   open: boolean,
 }> = ({ darkMode, changeMode, open }) => {
+  if (!open) { return null }
+
   const sun = <ImageIcon
     darkMode={darkMode}
     onClick={() => changeMode()}
@@ -29,10 +27,6 @@ const DarkModeToggle: React.FunctionComponent<{
     onClick={() => changeMode()}
     className="lnr lnr-moon"
   />;
-
-  if(!open){
-    return null;
-  }
 
   return darkMode ? sun : moon;
 };
